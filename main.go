862ab954aa71bb91afb76fdc3a6e38c83a3c1f98
AhmedAbdElfatah999 <asd701622@gmail.com>
2024-01-21 22:39:51 +0200

    Applying Clean Architecture

--- main.go
+++ main.go
@@ -3,28 +3,14 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"HelloWorld/routes"
-	"HelloWorld/db"
-	"fmt"
 )
 
 func main() {
-
-	// Initialize the database
-	dbInstance, err := db.InitDB()
-	if err != nil {
-		// Handle the error, log, and exit if necessary
-		panic(err)
-	}
-	defer dbInstance.Close()
-
-	// Set the global database instance
-	db.SetDBInstance(dbInstance)
-
 	// Create an instance of Echo
 	e := echo.New()
 
 	// Initialize routes
-	routes.InitRoutes(e,dbInstance)
+	routes.InitRoutes(e)
 
 	// Start the Echo server
 	e.Start(":8080")
--- routes/routes.go
+++ routes/routes.go
@@ -1,17 +1,32 @@
 package routes
 
 import (
-	"database/sql"
 	"github.com/labstack/echo/v4"
 	"HelloWorld/handlers"
+	"HelloWorld/database"
+	"HelloWorld/db"
+	"HelloWorld/api"
+
 )
 
+
+
 // InitRoutes initializes all routes
-func InitRoutes(e *echo.Echo, instance *sql.DB) {
+func InitRoutes(e *echo.Echo) {
+	sqlBuilder := database.NewSqlBuilder()
+// Initialize the database
+dbInstance, err := db.InitDB()
+if err != nil {
+	// Handle the error, log, and exit if necessary
+	panic(err)
+}
+//defer dbInstance.Close()
+productHandler := handlers.NewHandler(dbInstance, sqlBuilder)
+productRouter := api.NewProductRouter(productHandler)
 	// Product routes
-	e.POST("/products", handlers.CreateProductHandler)
-	e.GET("/products", handlers.ReadAllProductsHandler)
-	e.GET("/products/:id", handlers.ReadProductByIDHandler)
-	e.PUT("/products/:id", handlers.UpdateProductHandler)
-	e.DELETE("/products/:id", handlers.DeleteProductHandler)
+	e.POST("/products", productRouter.CreateProduct)
+	e.GET("/products", productRouter.ReadAll)
+	// e.GET("/products/:id", handlers.ReadProductByIDHandler)
+	// e.PUT("/products/:id", handlers.UpdateProductHandler)
+	// e.DELETE("/products/:id", handlers.DeleteProductHandler)
 }
